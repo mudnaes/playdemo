1f8e8fb1f737b16e1de57d5c5c28b0b75cca7e6a
morten <morten@cloud1.(none)>
2010-09-12 13:18:28 +0200

    Inital commit of tutorial

---
+++ yabe/app/controllers/Application.java
@@ -0,0 +1,26 @@
+package controllers;
+ 
+import java.util.*;
+ 
+import play.*;
+import play.mvc.*;
+ 
+import models.*;
+ 
+public class Application extends Controller {
+	
+	@Before
+	static void addDefaults() {
+	    renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+	    renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+	}
+ 
+    public static void index() {
+        Post frontPost = Post.find("order by postedAt desc").first();
+        List<Post> olderPosts = Post.find(
+            "order by postedAt desc"
+        ).from(1).fetch(10);
+        render(frontPost, olderPosts);
+    }
+ 
+}
